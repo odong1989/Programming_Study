3a4aa26a4149787fa3c103b55e35ccfbc49d6a84
kwunodong <kwunodong@gmail.com>
2021-01-29 23:57:47 +0900

    .

---
+++ ProgramLanguage/python/Doit점프투파이썬/04프로그램의 입력 및 출력/04-1 함수 선언 예제.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return (a+b)
+
+a=3
+b=5
+
+c=add(a, b)
+print(c)
---
+++ ProgramLanguage/python/Doit점프투파이썬/05파이썬-클래스,모듈,패키지,예외처리,내장함수,외장함수/05-2.1모듈-간단한 모듈 만들기 예제.py
@@ -0,0 +1,8 @@
+#mod1.py
+#py확장자로 만든 모든 파이썬 파일들이 모두 '모듈'이다.
+
+def add(a,b):
+    return a +b
+
+def sub(a,b):
+    return a-b
