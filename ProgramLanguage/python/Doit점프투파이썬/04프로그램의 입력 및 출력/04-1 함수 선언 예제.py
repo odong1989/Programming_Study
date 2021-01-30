fa60cfdd1f123676d8577348ab88889ea884db7e
kwunodong <kwunodong@gmail.com>
2021-01-30 23:54:56 +0900

    .

--- ProgramLanguage/python/Doit점프투파이썬/04프로그램의 입력 및 출력/04-1 함수 선언 예제.py
+++ ProgramLanguage/python/Doit점프투파이썬/04프로그램의 입력 및 출력/04-1 함수 선언 예제.py
@@ -1,8 +1,9 @@
-def add(a,b):
+def add(a,b):   #a,b를 '매개변수(parameter,함수에 입력으로 전달된 값을 받는 변수)'라고 부른다.
     return (a+b)
 
 a=3
 b=5
 
-c=add(a, b)
+c=add(a, b) #여기의 a,b는 '인수(arguments,함수를 호출할 때 전달하는 입력값)'
 print(c)
+  
