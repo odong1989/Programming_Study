6ba8c8b6f176d1ce3887750f7c04272b777a4e77
kwunodong <kwunodong@gmail.com>
2021-02-04 23:53:45 +0900

    .

---
+++ ProgramLanguage/java/책_명품자바/8장/ex1_page414.java
@@ -0,0 +1,11 @@
+import java.util.Vector;
+
+public class ex1_page414 {
+    public static void main(String[] args){
+        Vector v = new Vector();
+        v.add("Hello");
+        v.add(new Integer(4));
+        System.out.pirntln("벡터내의 요소 객체 수 : "+v.size());
+    }
+
+}
