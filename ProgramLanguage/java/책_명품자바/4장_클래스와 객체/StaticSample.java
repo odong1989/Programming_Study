447a7e3235ce7687c56b3cf9d8ff747a98f15604
kwunodong <kwunodong@gmail.com>
2021-03-10 23:58:22 +0900

    .

---
+++ ProgramLanguage/java/책_명품자바/4장_클래스와 객체/StaticSample.java
@@ -0,0 +1,16 @@
+//java  static 멤버의 선언
+
+class StaticSample{
+	int n; //non-static 필드
+	void g() { //non-static 메소드
+	//편의상 내용생략
+	}
+	
+	static int m; //static 필드 
+	static void f() {//static 메소드
+	//편의상 내용생략
+	}
+
+	public static void main(String[] args)
+	{}
+}
