129d7fe623e7c026671cae5b7a344174879a5461
kwunodong <kwunodong@gmail.com>
2021-04-13 23:48:44 +0900

    연습

--- AI(인공지능, 머신러닝, 딥러닝 포함)/실리콘밸리 데이터과학자가 알려주는 따라하며 배우는 데이터 과학/6_1 통계,올바른분석을 위한 틀.R
+++ AI(인공지능, 머신러닝, 딥러닝 포함)/실리콘밸리 데이터과학자가 알려주는 따라하며 배우는 데이터 과학/6_1 통계,올바른분석을 위한 틀.R
@@ -1,6 +1,16 @@
-#6.1.1. 수면제 효과 연구 예(102~105페이지지)
+#6.1.1. 수면제 효과 연구 예(102~105페이지)
 y<- sleep$extra[sleep$group==1]
 y
 
 summary(y)
 sd(y)
+
+par(mfrow=c(2,2))
+hist(y)
+boxplot(y)
+qqnorm(y);qqline(y)
+hist(y,prob=TRUE)
+lines(density((y)),lty=2)
+
+t.test(y)
+t.test(y,alternative = "greater")
