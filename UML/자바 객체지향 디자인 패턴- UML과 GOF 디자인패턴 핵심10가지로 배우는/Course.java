15f9c579ce0acb4d08b26b0fb1086bdb007f2c71
kwunodong <kwunodong@gmail.com>
2021-05-28 23:28:28 +0900

    연습

---
+++ UML/자바 객체지향 디자인 패턴- UML과 GOF 디자인패턴 핵심10가지로 배우는/Course.java
@@ -0,0 +1,15 @@
+
+//42page
+public class Course {
+    private String id;
+    private String name;
+    private int numOfStudents =0;
+
+    public void addStudent(){
+
+    }
+
+    public void deleteStudent(){
+        
+    }
+}
