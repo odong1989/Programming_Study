c907d786d0eff5545b88d80af11d0343c5102df1
kwunodong <kwunodong@gmail.com>
2021-06-02 23:22:48 +0900

    연습

---
+++ ProgramLanguage/python/책_파이썬 코딩 도장/UNIT32_람다표현식.py
@@ -0,0 +1,16 @@
+#람다 표현식아닌 def로 실시하는 경우
+
+def plus_ten(x):
+    return x+10
+
+print(plus_ten(1))
+#-------------------------
+
+#lambda는 아래와 같이 선언을 합니다. 
+#lambda 매개변수들 : 식
+lambda x:x +10
+
+#출력결과 : <function <lambda> at 0x000001AE39949B80>
+
+#-------------------------
+
