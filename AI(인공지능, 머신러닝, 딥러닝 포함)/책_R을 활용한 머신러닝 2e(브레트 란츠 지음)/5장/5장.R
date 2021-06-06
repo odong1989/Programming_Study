b6cca6f067434bc9bd27785d31e87060e52777f4
kwunodong <kwunodong@gmail.com>
2021-06-06 23:55:16 +0900

    연습-2단계:데이터탐색과준비- 2.1.랜덤샘플링(sample(), set.seed()함수 활용)

--- AI(인공지능, 머신러닝, 딥러닝 포함)/책_R을 활용한 머신러닝 2e(브레트 란츠 지음)/5장/5장.R
+++ AI(인공지능, 머신러닝, 딥러닝 포함)/책_R을 활용한 머신러닝 2e(브레트 란츠 지음)/5장/5장.R
@@ -23,5 +23,67 @@ curve(-x * log2(x) - (1 - x) * log2(1 - x),
 #데이터 출처 : credit.csv[UCI 머신러닝 데이터 저장소(함부르크 대학교 한스 호프만이 기부)]
 
 
-read.csv("C:/sourceTree/Programming_Study/AI(인공지능, 머신러닝, 딥러닝 포함)/책_R을 활용한 머신러닝 2e(브레트 란츠 지음)/5장/credit.csv")
+credit<-read.csv("C:/sourceTree/Programming_Study/AI(인공지능, 머신러닝, 딥러닝 포함)/책_R을 활용한 머신러닝 2e(브레트 란츠 지음)/5장/credit.csv")
+credit
+
+#str : 통해 obs(데이터 값 개수), variables(칼럼들 갯수)를 파악한다.
+str(credit)
+
+#채무불이행을 할 것 같은 이부 대출 특징에 대해 table()출력을 실시.
+#수표계좌잔고와 저축계좌잔고는 대출의 채무불이행을 파악&예측에 중요한 예측변수로 쓸 수 있다.
+table(credit$checking_balance) #checking_balance : 수표 계좌 잔고
+table(credit$savings_balance)  #savings_balance  : 저축 계좌 잔고
+
+
+#대출의 일부 특징은 요청된 대출기간&금액 등이 있다.
+#summary(credit)라고 하는 것보다 $를 활용해 해당 칼럼만 분석하게 하면 
+#해당칼럼내의 최대값/최소값 1사분면값등을 간결히 알수 있다.
+
+#대출금정보-대출기간(months_loan_duration)
+summary(credit$months_loan_duration) 
+#최소 4개월~최대72개월 기간의 중앙값은 18개월, 
+
+
+#대출금정보-대출금액(credit$amount)
+summary(credit$amount)
+#대출금의 중앙값은 2320.
+
+#default벡터 : 대출신청자가 채무불이행을 했는지 알려주는 flag역할.
+table(credit$default)
+#yes가300=30%의 대출자가 채무를 불이행함.
+
+
+#credit$default(채무불이행여부)를 정답으로 삼고, 
+#1000개의 데이터를 훈련용/태스트용으로 나누어 훈련(머신러닝학습)후 평가를 하여
+#채무불이행 위험이 높은 고객을 제외시키는 모델을 훈련(개발)시켜보자.
+
+#단계1.랜덤한 훈련 및 테스트 데이터셋 생성
+# : 한마디로 "1000개의 데이터를 훈련용/테스트용으로 분할한다"는 말이다. 
+#   본 코드에서는 90%는 훈련용, 10%는 테스트용으로 활용한다.
+
+#[참고]'4장-의사결정트리'에서 했던 것과 공통점 및 차이점
+#(1)공통점 : 훈련용/테스트용으로 데이터를 분할한다는 것.
+#(2)차이점 : 랜덤 샘플을 실시하느다는 것.(이를 위해 set.seed()&sample() 난수생성기를 활용한다. )
+#            4장의 경우 임의의 기준으로 정렬되어 있기에 바로 훈련/테스트용을 분할이 가능했다.
+#            반면에 5장의 경우 정렬이 안되어있어 4장처럼 했다간 망한다.
+#            만약 1~900번의 데이터는 작은 대출만 있고, 901~1000번은 큰 대출만 있다면 어찌될까?
+#            이는 엉뚱한 시험범위만 공부한 셈이 된다.
+#            따라서 랜덤하게 섞어서 작은대출~큰대출 모두 훈련할 수 있도록 해줘야 하는 것이다.
+
+
+#랜덤 샘플실시(이를 위해 set.seed()&sample() 난수생성기를 활용한다. )
+set.seed(123) # 123은 임의의 값이다.
+train_sample <-sample(1000,900) #1000개의 데이터중 900개를 훈련용으로 할당한다.
+str(train_sample)
+
+
+credit_train <-credit[train_sample,]
+credit_test <-credit[-train_sample,] #앞에 -연산자가 붙으면 해당데이터에 포함되지 않은 데이터만 포함하도록 설정한다.
+
+
+#Q:데이터가 랜덤하게 잘 들어갔는지의 판단기준은?
+#A:훈련용/테스트용 모두 각각 채무불이행의 비율이 30%이면 랜덤이 잘 이뤄진 것이다.
+
+prop.table(table(credit_train$default)) # prop.table() : %비율로 표현해준다.
+prop.table(table(credit_test$default))  # prop.table() : %비율로 표현해준다.
 
