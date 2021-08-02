4cd19023ddab9919c10263a0084a4d4ff3f29611
kwunodong <kwunodong@gmail.com>
2021-08-02 23:46:27 +0900

    연습

--- AI(인공지능, 머신러닝, 딥러닝 포함)/모두의 딥러닝(개정2판)/코드19_1_gan모델만들기.py
+++ AI(인공지능, 머신러닝, 딥러닝 포함)/모두의 딥러닝(개정2판)/코드19_1_gan모델만들기.py
@@ -10,3 +10,20 @@ Original file is located at
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout
 from tensorflow.keras.layers import BatchNormalization, Activation, LeakyReLU, UpSampling2D, Conv2D
+from tensorflow.keras.models import Sequential, Model
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#생성자 모델 만들기
+generator = Sequential()
+generator.add(Dense(128*7*7, input_dim=100, activation=LeakyReLU(0.2)))
+generator.add(BatchNormalization() )
+generator.add(Reshape((7,7,128)) )
+generator.add(UpSampling2D() )
+generator.add(Conv2D(64, kernel_size=5, padding='same', activation='tanh' ) )
+
+
+
+
+
